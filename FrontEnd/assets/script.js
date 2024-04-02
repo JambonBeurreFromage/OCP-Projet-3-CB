d020549f2ea96f9bc37c3c0145299fbe5ae31eb0
JambonBeurreFromage <103459767+JambonBeurreFromage@users.noreply.github.com>
2024-04-02 13:24:26 +0200

    Suite mentorat

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,7 +1,8 @@
 
 
 // Selection des éléments HTML
-let gallery = document.querySelector(".gallery")
+let selectorProject = document.querySelector(".selectorProject");
+let gallery = document.querySelector(".gallery");
 let showAllBtn = document.getElementById("selectorAll");
 let showObjectsBtn = document.getElementById("selectorObject");
 let showFlatBtn = document.getElementById("selectorFlat");
@@ -30,6 +31,28 @@ function createHTML(projects) {
 };
 
 
+//Fonction de changement de classe // Paramètre : Bouton selectionné
+function changeClass(btnSelect) {
+    //Réinitialise la classe montrant la selection du filtre
+    showAllBtn.classList.remove("selectorProjectSelect");
+    showObjectsBtn.classList.remove("selectorProjectSelect");
+    showFlatBtn.classList.remove("selectorProjectSelect");
+    showHotelBtn.classList.remove("selectorProjectSelect");
+
+    if (btnSelect === showAllBtn) {
+        showAllBtn.classList.add("selectorProjectSelect");
+
+    } else if (btnSelect === showObjectsBtn) {
+        showObjectsBtn.classList.add("selectorProjectSelect");
+    
+    } else if (btnSelect === showFlatBtn) {
+        showFlatBtn.classList.add("selectorProjectSelect");
+
+    } else if (btnSelect === showHotelBtn) {
+        showHotelBtn.classList.add("selectorProjectSelect");
+    }
+};
+
 
 //Fonction de gestion du filtre
 async function filterProjects() { 
@@ -38,20 +61,40 @@ async function filterProjects() {
     const allProjects = await fetch("http://localhost:5678/api/works/");
     const showAllProjets = await allProjects.json();
 
+    // //Création du filtre dynamique. Voir pour récupérer la valeur name de category
+    const filterSet = new Set();
+
+    console.log(showAllProjets.length)
+
+    for (i = 0; i < showAllProjets.length; i++) {
+
+        filterSet.add(showAllProjets[i].category)
+    }
+    console.log(filterSet)
+    console.log(showAllProjets.category)
+
     //Affiche tous les projets au premier chargement
     createHTML(showAllProjets);
 
     //Filtrer les projets
+    //Tous afficher
+    showAllBtn.addEventListener("click", () => {
+        changeClass(showAllBtn);
+        createHTML(showAllProjets);
+    });
+
     //Par objets
     showObjectsBtn.addEventListener("click", () => {
         const showObjects = showAllProjets.filter((project) => project.categoryId === 1);
-        createHTML (showObjects);
+        changeClass(showObjectsBtn);
+        createHTML(showObjects);
     });
 
 
     //Par appartements
     showFlatBtn.addEventListener("click", () => {
         const showFlat = showAllProjets.filter((project) => project.categoryId === 2);
+        changeClass(showFlatBtn);
         createHTML (showFlat);
     });
 
@@ -59,6 +102,7 @@ async function filterProjects() {
     //Par Hotels
     showHotelBtn.addEventListener("click", () => {
         const showHotel = showAllProjets.filter((project) => project.categoryId === 3);
+        changeClass(showHotelBtn);
         createHTML (showHotel);
     });
 }
